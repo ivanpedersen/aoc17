6951454ae87e3cb35043486a2547e17fc5ac74b5
Ivan Pedersen <ivan@alpe.se>
2017-12-05 21:21:03 +0100

    Day 4 part 2

--- 4/passphrase.go
+++ 4/passphrase.go
@@ -6,14 +6,47 @@ import "log"
 import "os"
 import "bytes"
 import "strings"
+import "sort"
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
 
 func valid_passphrase(b []byte) bool {
 	fields := strings.Fields(string(b[:]))
 	m := make(map[string]int)
 	for _, field := range fields {
 		m[field]++
-		fmt.Println("m: ", m)
 		if m[field] > 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func valid_passphrase_anagram(b []byte) bool {
+	fields := strings.Fields(string(b[:]))
+	m := make(map[string]int)
+	for _, field := range fields {
+		s := SortString(field)
+		m[s]++
+		if m[s] > 1 {
 			return false
 		}
 	}
@@ -24,6 +57,16 @@ func passphrase(b []byte) int {
 	result := 0
 	for _, row := range bytes.Split(b, []byte("\n")) {
 		if valid_passphrase(row) {
+			result += 1
+		}
+	}
+	return result
+}
+
+func passphrase_anagram(b []byte) int {
+	result := 0
+	for _, row := range bytes.Split(b, []byte("\n")) {
+		if valid_passphrase_anagram(row) {
 			result += 1
 		}
 	}
@@ -42,4 +85,5 @@ func main() {
 	}
 
 	fmt.Println("Part 1:", passphrase(buf))
+	fmt.Println("Part 2:", passphrase_anagram(buf))
 }
--- 4/passphrase_test.go
+++ 4/passphrase_test.go
@@ -30,3 +30,29 @@ func TestPassphrasePart1WithInputFile(t *testing.T) {
 		t.Errorf("Result was wrong, got: %d, want: %d.", Result, Want)
 	}
 }
+
+func TestPassphrasePart2WithTestInputFile(t *testing.T) {
+	pwd, _ := os.Getwd()
+	buf, err := ioutil.ReadFile(pwd + "/test_input_part2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Result := passphrase_anagram(buf)
+	Want := 3
+	if Result != Want {
+		t.Errorf("Result was wrong, got: %d, want: %d.", Result, Want)
+	}
+}
+
+func TestPassphrasePart2WithInputFile(t *testing.T) {
+	pwd, _ := os.Getwd()
+	buf, err := ioutil.ReadFile(pwd + "/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Result := passphrase_anagram(buf)
+	Want := 265
+	if Result != Want {
+		t.Errorf("Result was wrong, got: %d, want: %d.", Result, Want)
+	}
+}
